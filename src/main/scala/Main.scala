81e30e8ebec1cb89806b9c8114bccffb07412efa
Christopher Spears <cspears2002@gmail.com>
2023-07-09 11:09:18 -0700

    Added powerOf function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,18 @@
+import scala.math._
+
 @main def hello: Unit =
   println("Hello world!")
   println(msg)
 
   println(product("Hello"))
 
+  println(powerOf(2, 2))
+  println(powerOf(2, 3))
+  println(powerOf(2, 0))
+  println(powerOf(2, -1))
+
 def msg = "I was compiled by Scala 3. :)"
+
 
 def product(s: String): Long =
   var x: Long = 1
@@ -12,4 +20,19 @@ def product(s: String): Long =
     x * s.head.toLong
   else
     x * s.head.toLong * product(s.tail)
+
+
+def powerOf(x: Int, n: Int): Double =
+  val remainder = n % 2
+  if (n > 0) then
+    if (remainder == 0) then
+      val y = powerOf(x, n/2)
+      y * y
+    else
+      val diff = n - 1
+      x * powerOf(x, diff)
+  else if (n == 0) then
+    1
+  else
+    1 / powerOf(x, -1*n)
   
