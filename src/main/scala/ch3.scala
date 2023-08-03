4cb306e58244ad4819ba180dbe743e97e8664b86
Christopher Spears <cspears2002@gmail.com>
2023-08-03 10:56:58 -0700

    Wrote function to swap integers

--- src/main/scala/ch3.scala
+++ src/main/scala/ch3.scala
@@ -1,11 +1,26 @@
+import scala.collection.mutable.ArrayBuffer
+
 import scala.util.Random
 
 @main def mainFunc() =
-    println("Hello world!")
     val arrayOfInt = randArrayOfInts(5)
     arrayOfInt.foreach(println)
+
+    val swapped = swapInts(Array(1, 2, 3, 4, 5))
+    swapped.foreach(println)
+
 
 def randArrayOfInts(n: Int): Array[Int] =
     val rand = Random()
     val idxSeq = (0 to (n-1)).map(_ => rand.nextInt())
     idxSeq.toArray
+
+
+def swapInts(x: Array[Int]): Array[Int] = 
+    for i <- 0 until x.length - 1 by 2 do
+        val temp = x(i)
+        x(i) = x(i+1)
+        x(i+1) = temp
+    x
+
+    
