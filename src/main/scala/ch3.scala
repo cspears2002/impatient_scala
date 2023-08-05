678fde49ddf87ff090d08a5d23e5caed74dccab1
Christopher Spears <cspears2002@gmail.com>
2023-08-05 15:50:51 -0700

    Use for/yield to create new array

--- src/main/scala/ch3.scala
+++ src/main/scala/ch3.scala
@@ -9,6 +9,12 @@ import scala.util.Random
     val swapped = swapInts(Array(1, 2, 3, 4, 5))
     swapped.foreach(println)
 
+    val swapped2 = swapInts2(Array(1, 2, 3, 4, 5))
+    swapped2.foreach(println)
+
+    val myArray = Array(1, 0, -5, 2, 4)
+    // Want Array(1, 2, 4, 0, -5)
+
 
 def randArrayOfInts(n: Int): Array[Int] =
     val rand = Random()
@@ -23,4 +29,7 @@ def swapInts(x: Array[Int]): Array[Int] =
         x(i+1) = temp
     x
 
+
+def swapInts2(x: Array[Int]): Array[Int] = 
+    (for {y <- x.grouped(2); z <- y.reverse} yield z).toArray
     
