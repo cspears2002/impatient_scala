3be9d13d692f9922fd8b0aaf749b7ce4dd2136e3
Christopher Spears <cspears2002@gmail.com>
2023-08-13 11:58:32 -0700

    Function to remove all but first neg number

--- src/main/scala/ch3.scala
+++ src/main/scala/ch3.scala
@@ -5,21 +5,41 @@ import scala.util.Random
 @main def mainFunc() =
     val arrayOfInt = randArrayOfInts(5)
     arrayOfInt.foreach(println)
+    println("\n")
 
     val swapped = swapInts(Array(1, 2, 3, 4, 5))
     swapped.foreach(println)
+    println("\n")
 
     val swapped2 = swapInts2(Array(1, 2, 3, 4, 5))
     swapped2.foreach(println)
+    println("\n")
 
     val myArray = Array(1, 0, -5, 2, 4)
     // Want Array(1, 2, 4, 0, -5)
     val newArray = posToNeg(myArray)
     newArray.foreach(println)
+    println("\n")
 
     val arrayDbles: Array[Double] = Array(1.0, 2.0, 3.0, 4.0) 
     val avg = computeAvg(arrayDbles)
-    println(s"The average is $avg.")
+    println(s"The average is $avg.\n")
+
+    val myArray2 = Array(1, 5, 3, 2)
+    val sortedMyArray2 = myArray2.sorted
+    val revSortedMyArray2 = sortedMyArray2.reverse
+    revSortedMyArray2.foreach(println)
+    println("\n")
+
+    val arrayBuf = ArrayBuffer(5, 3, 2)
+    arrayBuf.sortInPlace
+    arrayBuf.reverse
+    arrayBuf.foreach(println)
+    println("\n")
+
+    val arrayBuf2 = ArrayBuffer(1, -1, 2, -4, 7, 6, -9)
+    val newArrayBuf = removeNegNum(arrayBuf2)
+    newArrayBuf.foreach(println)
 
 
 def randArrayOfInts(n: Int): Array[Int] =
@@ -49,3 +69,22 @@ def posToNeg(myArray: Array[Int]): Array[Int] =
 
 def computeAvg(myArray: Array[Double]): Double =
     myArray.sum / myArray.length
+
+
+def removeNegNum(myArrayBuffer: ArrayBuffer[Int]): ArrayBuffer[Int] =
+    /* This will remove all but the first negative number from
+    an array buffer.*/
+    val negIdx = ArrayBuffer[Int]()
+    for ((elem, i) <- myArrayBuffer.zipWithIndex) {
+        // println(s"$i: $elem")
+        if elem < 0 then
+            negIdx.append(i)
+    }
+    negIdx.remove(0)
+    val revNegIdx = negIdx.reverse
+    revNegIdx.foreach(println)
+    for (y <- revNegIdx) {
+        println(s"Remove $y")
+        myArrayBuffer.remove(y)
+    }
+    myArrayBuffer
