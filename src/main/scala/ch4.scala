63536a37194b41ea86938e4ae88d3e85e3fdc56b
Christopher Spears <cspears2002@gmail.com>
2023-09-04 11:10:28 -0700

    Use java.util.TreeMap

--- src/main/scala/ch4.scala
+++ src/main/scala/ch4.scala
@@ -4,19 +4,18 @@ import scala.io.Source
     readMyFile()
 
 def readMyFile(): Unit =
-    var wordCount = scala.collection.SortedMap[String, Int]()
+    var wordCount = java.util.TreeMap[String, Int]()
 
     var source = Source.fromFile("myfile.txt")
     var lineIterator = source.getLines()
     for l <- lineIterator do
         for w <- l.split("\\s+") do
             val word = w.replaceAll("\\p{Punct}", "")
-            if wordCount.contains(word) then
-                val count = wordCount(word) + 1
-                wordCount = wordCount + (word -> count)
+            if wordCount.containsKey(word) then
+                val count = wordCount.get(word) + 1
+                wordCount.put(word, count)
             else
-                wordCount = wordCount + (word -> 1)
+                wordCount.put(word, 1)
     
-    for (k, v) <- wordCount do
-        println(s"$k: $v")
+    wordCount.forEach((k, v) => println(s"$k: $v"))
 
