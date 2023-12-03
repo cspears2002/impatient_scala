de408785011a545db7e38eafb7a77429dcf5de0e
Christopher Spears <cspears2002@gmail.com>
2023-12-03 14:18:14 -0800

    Test the conversion methods

--- src/main/scala/Conversions.scala
+++ src/main/scala/Conversions.scala
@@ -10,3 +10,12 @@ object Conversions {
   def milesToKilometers(miles: Double): Double =
     miles * 1.60934
 }
+
+
+@main def convertStuff(): Unit =
+    val cm = Conversions.inchesToCentimeters(1)
+    println(s"1 inch = $cm cm")
+    val liters = Conversions.gallonsToLiters(1)
+    println(s"1 gallon = $liters liters")
+    val km = Conversions.milesToKilometers(1)
+    println(s"1 mile = $km km")
