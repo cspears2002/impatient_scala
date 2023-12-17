55280298f67c38942284cbcf112815d219bf8128
Christopher Spears <cspears2002@gmail.com>
2023-12-17 15:00:43 -0800

    Set the color of a suit

--- src/main/scala/CardSuits.scala
+++ src/main/scala/CardSuits.scala
@@ -1,13 +1,25 @@
-enum Suit(val symbol: String):
-    case Spades extends Suit("\u2660")
-    case Diamonds extends Suit("\u2666")
-    case Clubs extends Suit("\u2663")
-    case Hearts extends Suit("\u2764")
+enum SuitColor:
+    case Black
+    case Red
+
+enum Suit(val symbol: String, color: SuitColor):
+    case RedSpades extends Suit("\u2660", SuitColor.Red)
+    case RedDiamonds extends Suit("\u2666", SuitColor.Red)
+    case RedClubs extends Suit("\u2663", SuitColor.Red)
+    case RedHearts extends Suit("\u2764", SuitColor.Red)
+    case BlackSpades extends Suit("\u2660", SuitColor.Black)
+    case BlackDiamonds extends Suit("\u2666", SuitColor.Black)
+    case BlackClubs extends Suit("\u2663", SuitColor.Black)
+    case BlackHearts extends Suit("\u2764", SuitColor.Black)
     override def toString(): String = 
         symbol
+    def getColor(): String = color.toString()
 
 @main def playCards() =
-    println(Suit.Spades.toString())
-    println(Suit.Diamonds.toString())
-    println(Suit.Clubs.toString())
-    println(Suit.Hearts.toString())
+    println(Suit.RedSpades.toString())
+    println(Suit.RedDiamonds.toString())
+    println(Suit.RedClubs.toString())
+    println(Suit.RedHearts.toString())
+    
+    println(Suit.RedClubs.getColor())
+    println(Suit.BlackClubs.getColor())
