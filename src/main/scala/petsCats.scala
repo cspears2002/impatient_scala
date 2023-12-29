0706597bc948f9b149cfccd7aa251817effc02e0
Christopher Spears <cspears2002@gmail.com>
2023-12-28 16:00:21 -0800

    Create a pets package

---
+++ src/main/scala/petsCats.scala
@@ -0,0 +1,8 @@
+package pets
+
+class Siamese(val name: String)
+
+class Bombay(val name: String)
+
+def meow(): Unit =
+    println("Meow!")
---
+++ src/main/scala/petsDogs.scala
@@ -0,0 +1,8 @@
+package pets
+
+class AustralianShepherd(val name: String)
+
+class BorderCollie(val name: String)
+
+def bark(): Unit =
+    println("Ruff!")
