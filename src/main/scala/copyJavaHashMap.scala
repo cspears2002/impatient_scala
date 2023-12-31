3a503a1f48c23dca03f9aaa547cb696a8f34b4b5
Christopher Spears <cspears2002@gmail.com>
2023-12-30 16:12:03 -0800

    Copy java map to scala map

--- src/main/scala/copyJavaHashMap.scala
+++ src/main/scala/copyJavaHashMap.scala
@@ -7,7 +7,8 @@ import scala.collection.mutable.HashMap as scalaHashMap
     javaHashMap.put("b", 2)
     javaHashMap.put("c", 3)
 
-    val scalaMap = scalaHashMap[String, Int]
-    javaHashMap.forEach((k, v) => println(s"${k}, ${v}"))
+    val scalaMap = scalaHashMap.empty[String, Int]
+    javaHashMap.forEach((k, v) => scalaMap.put(k, v))
+    scalaMap.foreach((k, v) => println(s"${k} => ${v}"))
 
     
