65ceba6fb27dc5c03c9f793b3c98f47f6c2d0cf7
Christopher Spears <cspears2002@gmail.com>
2024-01-02 09:44:38 -0800

    Finished creating stack

--- src/main/scala/stack.scala
+++ src/main/scala/stack.scala
@@ -3,9 +3,20 @@ import scala.collection.mutable.ArrayBuffer
 class MyStack(val buf: ArrayBuffer[String] = ArrayBuffer[String]()):
     export buf.{addOne, length, remove}
 
-    def push(elem: String): ArrayBuffer[String] = 
+    def push(elem: String): MyStack = 
         buf.addOne(elem)
+        this
 
     def pop(): String =
-        buf.remove(buf.length)
+        buf.remove(buf.length - 1)
 
+
+@main def createStack(): Unit =
+    val myStack = MyStack()
+    println(s"Stack length is ${myStack.length}")
+    println("Add an element: 'a string'")
+    myStack.push("a string")
+    println(s"Stack length is ${myStack.length}")
+    val elem = myStack.pop()
+    println(s"Pop off an element: ${elem}")
+    println(s"Stack length is ${myStack.length}")
